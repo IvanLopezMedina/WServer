456571ad94dd18b2a2a025e6148de4a11879dc0d
IvanLopezMedina <ivan.lopez.medina.ilm@gmail.com>
2018-03-06 16:28:41 +0100

    ascihtml

---
+++ src/AsciiInputStream.java
@@ -0,0 +1,37 @@
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+//Notas: Sobreescribir el metodo read. Envez de leer un caracter sobrecargar
+public class AsciiInputStream extends FilterInputStream {
+
+    /**
+     * Creates a <code>FilterInputStream</code>
+     * by assigning the  argument <code>in</code>
+     * to the field <code>this.in</code> so as
+     * to remember it for later use.
+     *
+     * @param in the underlying input stream, or <code>null</code> if
+     *           this instance is to be created without an underlying stream.
+     */
+
+    protected AsciiInputStream(InputStream in) {
+        super(in);
+        this.in = in;
+
+    }
+    public int read() throws IOException {
+
+        int c = this.in.read();
+        if (c == -1) return -1;
+        if (c == '<') {
+            int c2;
+            while ((c2 = this.in.read()) != '>') {
+                if (c2 == -1) return -1; //cuando se llega al final
+            }
+            return this.read();
+        }
+        return c;
+    }
+}
