2080b055c24b49e9a0c2577658dd0d225c21049c
Iván López Medina <ivan.lopez.medina.ilm@gmail.com>
2018-03-06 13:17:13 +0100

    Overwrite read

--- src/AsciiInputStream.java
+++ src/AsciiInputStream.java
@@ -1,8 +1,33 @@
 import java.io.FilterInputStream;
+import java.io.InputStream;
 
 public class AsciiInputStream extends FilterInputStream{
 
 
-    public void 
+    /**
+     * Creates a <code>FilterInputStream</code>
+     * by assigning the  argument <code>in</code>
+     * to the field <code>this.in</code> so as
+     * to remember it for later use.
+     *
+     * @param in the underlying input stream, or <code>null</code> if
+     *           this instance is to be created without an underlying stream.
+     */
+    protected AsciiInputStream(InputStream in) {
+        super(in);
+        AsciiInputStream asc;
+
+
+    }
+
+
+    public int read(byte b[]){
+        FilterInputStream f;
+        //f.read();
+
+    }
+    public void convertToAsci(){
+//Sobreescribir el metodo read. Envez de leer un caracter, sobrecargar.
+    }
 
 }
