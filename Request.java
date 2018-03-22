2740fa0d8da6d91c5421895b30cffeb0635a41cc
Iván López Medina <ivan.lopez.medina.ilm@gmail.com>
2018-03-22 09:12:40 +0100

    Auto stash before merge of "TestZips" and "origin/TestZips"

--- Request.java
+++ Request.java
@@ -24,9 +24,7 @@ public class Request {
         }
     }
 
-    boolean isAsc(){
-        return asc;
-    }
+    boolean isAsc(){ return asc; }
 
     boolean isZip(){
         return zip;
@@ -64,15 +62,15 @@ public class Request {
                 } else if (filename.contains("txt")) {
                     tipus = "text/plain";
                 }
-                if (zip == true) {
+                if (zip) {
                     tipus = "applications/zip";
                 }
-                if (gzip == true) {
+                if (gzip) {
                     tipus = "applications/x-gzip";
                 }
 
                 header_response.print("Content-Type: " + tipus + "\n");
-                if ((zip || gzip)){
+                if (zip || gzip || (!tipus.contains("text")&&!tipus.contains("html"))){
                     header_response.print("Content-Disposition: filename=\"" + filename.substring(1));
                     if (asc && (header.contains("txt") || header.contains("html"))) header_response.print(".asc");
                 }
@@ -82,7 +80,7 @@ public class Request {
                     System.out.println(file.length());
                 } else if (!zip && gzip) {
                     header_response.print( ".gz\"\n");
-                    header_response.print("Content-Length: " + (int) file.length()+1 + "\n\n");
+                    header_response.print("Content-Length: " + (int) file.length() + "\n\n");
                 } else if (zip && gzip) {
                     header_response.print( ".zip.gz\"\n");
                     if (asc && (header.contains("txt") || header.contains("html"))) header_response.print(".asc");
